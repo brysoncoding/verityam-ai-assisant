8998694bb7e9e2377d579ea054a055d724f2e0aa
brysoncoding <bryson.levi.comfort@gmail.com>
2026-08-07 14:03:29 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "./components/Header";
 import Chat, { ChatMessage } from "./components/Chat";
