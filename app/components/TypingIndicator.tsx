287aa36f0801101f3b2c4b3fb94e5ebc4b09ae52
brysoncoding <bryson.levi.comfort@gmail.com>
2026-08-07 12:13:30 -0400

    Create TypingIndicator.tsx

---
+++ app/components/TypingIndicator.tsx
@@ -0,0 +1,14 @@
+export default function TypingIndicator() {
+  return (
+    <div className="message ai">
+      <strong>VERITY</strong>
+      <br />
+
+      <div className="typing">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+}
