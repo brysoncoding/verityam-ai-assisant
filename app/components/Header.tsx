00b900ae80c10827fbb1b149bf4fc0a44ca5127a
brysoncoding <bryson.levi.comfort@gmail.com>
2026-08-07 12:09:42 -0400

    Create Header.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div>
+        <h1 className="title glow">VERITY</h1>
+        <p className="subtitle">
+          Signal received • ONLINE
+        </p>
+      </div>
+    </header>
+  );
+}
