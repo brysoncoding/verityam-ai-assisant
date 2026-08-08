b72f921f9305093c6f057b56ad0c84256812a2a6
brysoncoding <bryson.levi.comfort@gmail.com>
2026-08-08 10:06:31 -0400

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="header">
       <div>
-        <h1 className="title glow">VERITY</h1>
+        <h1 className="title glow">ECHO</h1>
         <p className="subtitle">
           Signal received • ONLINE
         </p>
